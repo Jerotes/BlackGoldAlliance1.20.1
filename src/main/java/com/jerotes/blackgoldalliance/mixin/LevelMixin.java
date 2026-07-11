fdceb6a746f8d51a49716af525d80fecb40f3bc2
Jerotes <2694429769@qq.com>
2026-07-11 14:16:16 +0800

    Changes

--- src/main/java/com/jerotes/blackgoldalliance/mixin/LevelMixin.java
+++ src/main/java/com/jerotes/blackgoldalliance/mixin/LevelMixin.java
@@ -16,11 +16,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import java.util.List;
 
 @Mixin(Level.class)
-public class LevelMixin extends CapabilityProvider<Level> {
-    protected LevelMixin(Class<Level> baseClass) {
-        super(baseClass);
-    }
-
+public class LevelMixin {
     @Inject(method = "destroyBlock(Lnet/minecraft/core/BlockPos;ZLnet/minecraft/world/entity/Entity;I)Z", at = @At("HEAD"), cancellable = true)
     private void destroyBlock(BlockPos pos, boolean dropBlock, Entity entity, int recursion, CallbackInfoReturnable<Boolean> cir) {
         if (entity instanceof LivingEntity livingEntity && livingEntity.hasEffect(BGAMobEffects.PIGLIN_DETERRENT.get())) {
